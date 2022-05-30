a40b98b92ecfc9794dea461418912310eff1c4f4
Aimee Sharp <sharpaim23@gmail.com>
2022-05-30 13:28:54 -0500

    Added index.ejs for Star Wars Quote App

--- server.js
+++ server.js
@@ -8,25 +8,35 @@ const connectionString = 'mongodb+srv://twodimes:Mizzou23@cluster0.jvzkg.mongodb
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.set('view engine', 'ejs')
+
+//Middlewares and other routes here...
+//Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.get('/',(req, res) => {
+      quotesCollection.find().toArray()
+      .then(results => {
+        console.log(results);
+      })
+      .catch(error => console.error(error))
+      res.sendFile(__dirname + '/index.html')
+      //Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    })
+    app.post('/quotes', (req,res) => {
+      quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+    })
+    app.listen(3000, function(){
+      console.log('listening on 3000');
+    })
   })
   .catch(error => console.error(error))
-  
-//Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({extended: true}))
 
-//All your handlers here...
-app.listen(3000, function(){
-  console.log('listening on 3000');
-})
 
-//We normally abbreviate `request` to `req` and `response` to `res`.
-app.get('/',(req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  //Note: __dirname is the current directory you're in. Try logging it and see what you get!
-  //Mine was C:\Users\twodi\devClass\StarwarsApp for this app!
-})
-
-app.post('/quotes', (req,res) => {
-  console.log(req.body);
-})
 
