c0d41526da965d51995e99161b271191ec160c2c
Aimee Sharp <sharpaim23@gmail.com>
2022-05-30 16:04:20 -0500

    Finished code for the index.ejs and added style.css for Star Wars Quote App

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { query } = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://twodimes:Mizzou23@cluster0.jvzkg.mongodb.net/?retryWrites=true&w=majority'
@@ -16,6 +17,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 //Middlewares and other routes here...
 //Make sure you place body-parser before your CRUD handlers!
     app.use(bodyParser.urlencoded({extended: true}))
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
     app.get('/',(req, res) => {
       quotesCollection.find().toArray()
       .then(results => {
@@ -28,6 +31,37 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
       quotesCollection.insertOne(req.body)
       .then(result => {
         res.redirect('/');
+      })
+      .catch(error => console.error(error))
+    })
+      app.put('/quotes', (req,res) => {
+      quotesCollection.findOneAndUpdate(
+        {name: 'Yoda'},
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+      .then(result => {
+        res.json('Success');
+      })
+      .catch(error => console.error(error))
+    })
+
+    app.delete('/quotes',(req,res) =>{
+      quotesCollection.deleteOne(
+        {name: req.body.name},
+      )
+      .then(result => {
+        if(result.deletedCount === 0){
+          return res.json('No quote to delete')
+        }
+        res.json("Deleted Darth Vader's quote")
       })
       .catch(error => console.error(error))
     })
